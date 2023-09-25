dcb868d79dc3edc782a5c7ba27168f93f6ca3a43
fabioprog1 <144070779+fabioprog1@users.noreply.github.com>
2023-09-25 23:49:04 +0000

    Parte 1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+
+      <div className="tags">
+        <span className="tag">Status: Vivo</span>
+        <span className="tag">Espécie: Humana</span>
+        <span className="tag">Origem: Terra C-137</span>
+      </div>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
+    </>
+  )
+}
+
+export default App
