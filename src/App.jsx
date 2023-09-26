785a87a9eb07d4b3f039b96a020f51ee62201275
fabioprog1 <144070779+fabioprog1@users.noreply.github.com>
2023-09-26 00:57:07 +0000

    Criação Card2

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,16 @@ function App() {
     nome: 'Rick Sanches',
     imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
+
+  const item2 = {
+    nome: 'Morty Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
   
   return (
     <>
       <Card item={item1} />
-      
+      <Card item={item2} />
     </>
   )
 }
